a150044257da4a7757bfb5ad5cdbf701c8573250
David Alvarez-mac <davidalvarezj@gmail.com>
2022-11-03 10:18:40 -0500

    Testing youtube API using fetch()

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,13 +1,13 @@
-const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCw05fUBPwmpu-ehXFMqfdMw&part=snippet%2Cid&order=date&maxResults=9';
+const API = 'https://youtube-v2.p.rapidapi.com/channel/videos?channel_id=UCAPByrKU5-R1emswVlyH_-g';
 
 const content = null || document.getElementById('content');
 
 const options = {
-  method: 'GET',
-  headers: {
-    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
-    'X-RapidAPI-Key': 'c68edbdba9mshc3cb6eb8038f3b2p1f444ajsna5de19fe1256'
-  }
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'cf2f94428cmsh50ae6715638f428p1388c1jsn76b159539574',
+		'X-RapidAPI-Host': 'youtube-v2.p.rapidapi.com'
+	}
 };
 
 async function fetchData(urlApi) {
@@ -19,24 +19,28 @@ async function fetchData(urlApi) {
 (async () => {
   try {
     const videos = await fetchData(API);
+
     let view = `
-    ${videos.items.map(video => `
+    ${videos.videos.map(video => `
+    <a href="https://www.youtube.com/watch?v=${video.video_id}" target="_blank">
       <div class="group relative">
         <div
           class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-          <img src="${video.snippet.thumbnail.high.url}" alt="${video.snippet.description}" class="w-full">
+          <img src="${video.thumbnails[3].url}" alt="${video.title}" class="w-full">
         </div>
         <div class="mt-4 flex justify-between">
           <h3 class="text-sm text-gray-700">
             <span aria-hidden="true" class="absolute inset-0"></span>
-            ${video.snippet.title}
+            ${video.title}
           </h3>
         </div>
       </div>
-    `).slice(0,4).join('')}
-
+    </a>
+    `).slice(0,20).join('')}
     `;
-  } catch {
-
+    console.log(view)
+    content.innerHTML = view
+  } catch (error){
+    console.log(error)
   }
 })();
